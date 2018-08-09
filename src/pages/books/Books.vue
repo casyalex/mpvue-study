1a27444df2cba513e8202e312bdffab6fb060ea4
xyx <superalex_1104@163.com>
2018-08-09 15:17:14 +0800

    底部导航路由配置 main.js的app Json config

---
+++ src/pages/books/Books.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    图书页面
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
---
+++ src/pages/comments/Comments.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    评论页面
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+

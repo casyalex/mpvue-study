628f6edcd44b29e8e9c8a20b67b7ce85f33d19aa
alex <superalex_1104@163.com>
2018-08-09 23:03:38 +0800

    小程序登录接口改变 userLogin废弃

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script>
 import {get} from './util'
+import qcloud from 'wafer2-client-sdk'
+import config from '@/config'
 export default {
   async created () {
     // wx.request({
@@ -8,6 +10,7 @@ export default {
     //     console.log(res)
     //   }
     // })
+    qcloud.setLoginUrl(config.loginUrl)
     const res = await get('/weapp/demo')
     console.log(res.data)
     console.log('小程序启动了')
--- src/pages/me/Me.vue
+++ src/pages/me/Me.vue
@@ -1,16 +1,76 @@
 <template>
-  <div>
-    个人中心
+  <div class="container">
+    <div class="userinfo" >
+      <img :src="userinfo.avatarUrl" alt="">
+      <p>{{userinfo.nickName}}</p>
+    </div>
+    <button v-if='userinfo.openId' @click='scanBook' class='btn'>添加图书</button>
+    <button v-else open-type="getUserInfo" lang="zh_CN" class='btn' @getuserinfo="login">点击登录</button>
+    <!-- <button open-type="getUserInfo" lang="zh_CN" bindgetuserinfo="doLogin">获取用户信息</button> -->
   </div>
 </template>
-
 <script>
+// import qcloud from 'wafer2-client-sdk'
+// import config from '@/config'
 export default {
-
+  data () {
+    return {
+      userinfo: {
+        avatarUrl: 'http://image.shengxinjing.cn/rate/unlogin.png',
+        nickName: ''
+      }
+    }
+  }
+  // methods: {
+  //   getWxLogin: function ({encryptedData, iv, userinfo}) {
+  //     const self = this
+  //     wx.login({
+  //       success: function (loginResult) {
+  //         console.log('loginResult', loginResult)
+  //         var loginParams = {
+  //           code: loginResult.code,
+  //           encryptedData: encryptedData,
+  //           iv: iv
+  //         }
+  //         qcloud.setLoginUrl(config.loginUrl)
+  //         qcloud.requestLogin({
+  //           loginParams,
+  //           success () {
+  //             // 获取用户信息
+  //             qcloud.request({
+  //               url: config.userUrl,
+  //               login: true,
+  //               success (userRes) {
+  //                 wx.setStorageSync('userinfo', userRes.data.data)
+  //                 self.userinfo = userRes.data.data
+  //               }
+  //             })
+  //           },
+  //           fail (error) {
+  //             console.log(error)
+  //             // showModal('登录失败', error)
+  //           }
+  //         })
+  //       },
+  //       fail: function (loginError) {
+  //         // showModal('登录失败', loginError)
+  //       }
+  //     })
+  //   }
+  // }
 }
 </script>
 
-<style lang="stylus" scoped>
+<style lang='stylus'>
+.container
+  padding: 0 30rpx
 
+.userinfo
+  margin-top: 100rpx
+  text-align: center
+  img
+    width: 150rpx
+    height: 150rpx
+    margin: 20rpx
+    border-radius: 50%
 </style>
-

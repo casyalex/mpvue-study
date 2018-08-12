67d0880b2e3c42f1d2d649b4b4f44f36c2815dac
alex <superalex_1104@163.com>
2018-08-12 10:39:23 +0800

    数据库查询调取 列表图书首页展示

--- src/pages/books/Books.vue
+++ src/pages/books/Books.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    图书页面
+    <div v-for="book in books" :key="book.id">{{book.title}}</div>
   </div>
 </template>
 
 <script>
+import {get} from '@/util'
 export default {
-
+  data () {
+    return {
+      books: []
+    }
+  },
+  methods: {
+    async getList () {
+      const books = await get('/weapp/booklist')
+      console.log(books)
+      this.books = books.list
+    }
+  },
+  mounted () {
+    this.getList()
+  }
 }
 </script>
 

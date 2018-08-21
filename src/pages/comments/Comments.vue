20fcd5df0625c98b9e6e47473a179445a75dfc8a
Alex <superalex_1104@163.com>
2018-08-21 16:05:37 +0800

    评论里的图书列表

--- src/pages/comments/Comments.vue
+++ src/pages/comments/Comments.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="container">
-    <CommentList type="user" :comments="comments"></CommentList>
+    <CommentList v-if="userinfo.openId" type="user" :comments="comments"></CommentList>
+    <div v-if="userinfo.openId">
+      <div class="page-title">
+        我的图书
+      </div>
+      <card v-for="book in books" :key="book.id" :book="book"></card>
+      <div v-if="!books.length">暂时还没添加过书，去添加几本把~</div>
+    </div>
   </div>
 </template>
 
 <script>
 import {get} from '@/util'
+import card from '@/components/Card'
 import CommentList from '@/components/CommentList'
 export default {
   components: {
-    CommentList
+    CommentList,
+    card
   },
   data () {
     return {
       comments: [],
+      books: [],
       userinfo: {}
     }
   },
@@ -21,6 +31,14 @@ export default {
     init () {
       wx.showNavigationBarLoading()
       this.getComments()
+      this.getBooks()
+      wx.hideNavigationBarLoading()
+    },
+    async getBooks () {
+      const books = await get('/weapp/booklist', {
+        openid: this.userinfo.openId
+      })
+      this.books = books.list
     },
     async getComments () {
       const comments = await get('/weapp/commentlist', {
@@ -28,6 +46,10 @@ export default {
       })
       this.comments = comments.list
     }
+  },
+  onPullDownRefresh () {
+    this.init()
+    wx.stopPullDownRefresh()
   },
   onShow () {
     if (!this.userinfo.openId) {

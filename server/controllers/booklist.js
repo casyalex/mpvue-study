91c22f36c50df1382e5876a6c472541a49888df7
alex <superalex_1104@163.com>
2018-08-21 22:20:22 +0800

    图书列表server api修改

--- server/controllers/booklist.js
+++ server/controllers/booklist.js
@@ -1,14 +1,20 @@
 const { mysql } = require('../qcloud')
 
 module.exports = async (ctx) => {
-    const {page} = ctx.request.query
+    const {page, openid} = ctx.request.query
     const size = 10
-    const books = await mysql('books')
+    const mysqlSelect = mysql('books')
                           .select('books.*', 'cSessionInfo.user_info')
                           .join('cSessionInfo', 'books.openid', 'cSessionInfo.open_id')
-                          .limit(size)
-                          .offset(Number(page) * size)
                           .orderBy('books.id', 'desc')
+    let books
+    if (openid) {
+      // 根据openid过滤
+        books = await mysqlSelect.where('books.openid', openid)
+    } else {
+      // 全部图书分页
+        books = await mysqlSelect.limit(size).offset(Number(page) * size)
+    }
     ctx.state.data = {
         list: books.map(v => {
             const info = JSON.parse(v.user_info)
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // 配置项
-const host = 'http://192.168.8.172:5757'
+const host = 'http://localhost:5757'
 
 const config = {
   host,
